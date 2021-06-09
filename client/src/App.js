301c2ee7b91673f1fec41b124db1d5f1e9b5ed2e
billybep <billy.pesoth@gmail.com>
2021-06-09 23:11:42 +0700

    add page home, portfolio & about + install materialUI

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
+import Home from '../src/pages/Home'
+import About from '../src/pages/About'
+import Portfolio from '../src/pages/Portfolio'
 
 function App() {
   return (
     <div className="App">
-      <h1>
-        Billy Pesoth Home Page
-      </h1>
+      <Switch>
+        <Route path='/'           component={Home} exact/>
+        <Route path='/about'      component={About} />
+        <Route path='/portfolio'  component={Portfolio} />
+      </Switch>
     </div>
   );
 }
